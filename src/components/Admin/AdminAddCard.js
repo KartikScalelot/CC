9762fb8a5bc9e54122cebf108ccf158e0c6c64a8
Kartik Jetani <kartikjetani15@gmail.com>
2023-05-09 17:52:59 +0530

    front/back card

--- src/components/Admin/AdminAddCard.js
+++ src/components/Admin/AdminAddCard.js
@@ -29,7 +29,9 @@ function AdminAddCard() {
 		card_number: "",
 		card_holder_name: "",
 		card_network: "",
-		card_photo: "",
+		// card_photo: "",
+		frontside_card_photo: "",
+		backside_card_photo: "",
 		card_exp_date: "",
 		card_cvv: "",
 		available_balance: "",
@@ -42,7 +44,8 @@ function AdminAddCard() {
 		card_number: Yup.string().test('len', 'Must be exactly 12 characters', val => val.length === 12).required('Card number is required*'),
 		card_holder_name: Yup.string().required('Card holder name is required*'),
 		card_network: Yup.string().required('Card Network is required*'),
-		card_photo: Yup.string().required('Card photo is required*'),
+		frontside_card_photo: Yup.string().required('Frontside Card photo is required*'),
+		backside_card_photo: Yup.string().required('Backside Card photo is required*'),
 		card_exp_date: Yup.date().required('Expiry date is required*'),
 		card_cvv: Yup.string().test('len', 'Must be exactly 3 characters', val => val.length === 3).required('Cvv is required*'),
 		available_balance: Yup.number().positive('Available balance should be greater than 0*'),
@@ -134,12 +137,12 @@ function AdminAddCard() {
 									<option value="Credit Card">Credit Card</option>
 									<option value="Debit Card">Debit Card</option>
 								</select> */}
-							{/* <select type="text" name="card_type" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Select Card Type' required onChange={(e) => setInputValue("card_type", e.target.value)}>
+							{/* <select type="text" name="card_type" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Select Card Type' onChange={(e) => setInputValue("card_type", e.target.value)}>
 									<option value="Credit Card">Credit Card</option>
 									<option value="Debit Card">Debit Card</option>
 								</select> */}
 
-							{/* <input type="text" name="card_type" className="input_box2 placeholder:text-[#94A3B8]text-sm sm: placeholder:text-xl" placeholder='Select card' required onChange={(e) => setInputValue("card_type", e.target.value)} /> */}
+							{/* <input type="text" name="card_type" className="input_box2 placeholder:text-[#94A3B8]text-sm sm: placeholder:text-xl" placeholder='Select card' onChange={(e) => setInputValue("card_type", e.target.value)} /> */}
 							{/* <small className="text-red-500 text-xs">{formik.errors.card_type}</small>
 							</div> */}
 							{/* <div className={'w-full sm:w-1/2 mb-4 sm:mb-0' + (formik.values.card_type === "Debit Card" ? "opacity-30" : "")}> */}
@@ -172,24 +175,24 @@ function AdminAddCard() {
 						<div className="w-full flex flex-wrap sm:flex-nowrap sm:mb-7 sm:space-x-6">
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_bank_name" className="input-title2">Card bank name*</label>
-								<input type="text" name="card_bank_name" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter bank name' required onChange={(e) => setInputValue("card_bank_name", e.target.value)} />
+								<input type="text" name="card_bank_name" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter bank name' onChange={(e) => setInputValue("card_bank_name", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_bank_name}</small>
 							</div>
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_number" className="input-title2">Card Number*</label>
-								<input maxLength={12} type="text" name="card_number" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card number' required onChange={(e) => setInputValue("card_number", e.target.value)} />
+								<input maxLength={12} type="text" name="card_number" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card number' onChange={(e) => setInputValue("card_number", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_number}</small>
 							</div>
 						</div>
 						<div className="w-full flex flex-wrap sm:flex-nowrap sm:mb-7 sm:space-x-6">
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_holder_name" className="input-title2">Card holder name*</label>
-								<input type="text" name="card_holder_name" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card holder name' required onChange={(e) => setInputValue("card_holder_name", e.target.value)} />
+								<input type="text" name="card_holder_name" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card holder name' onChange={(e) => setInputValue("card_holder_name", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_holder_name}</small>
 							</div>
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_network" className="input-title2">Card network*</label>
-								<input type="text" name="card_network" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Visa' required onChange={(e) => setInputValue("card_network", e.target.value)} />
+								<input type="text" name="card_network" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Visa' onChange={(e) => setInputValue("card_network", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_network}</small>
 							</div>
 						</div>
@@ -215,7 +218,7 @@ function AdminAddCard() {
 							</div> */}
 							<div className='flex flex-wrap md:flex-nowrap items-center w-full md:w-1/2 md:space-x-3'>
 								<div className='w-full md:w-1/2'>
-									<label htmlFor="card_photo" className="input-title2">Card Front photo*</label>
+									<label htmlFor="frontside_card_photo" className="input-title2">Card Front photo*</label>
 									<label className='input_box2 flex items-center border-dashed justify-start py-2' htmlFor='card-photo'>
 										<svg width="22" height="17" viewBox="0 0 22 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 											<path fillRule="evenodd" clipRule="evenodd" d="M10.4444 1.75C7.65101 1.75 5.35585 3.88704 5.10594 6.6149C5.07 7.0073 4.74063 7.306 4.34837 7.3056C2.9362 7.3044 1.75 8.4797 1.75 9.8889C1.75 11.3156 2.9066 12.4722 4.33333 12.4722H5C5.41421 12.4722 5.75 12.808 5.75 13.2222C5.75 13.6364 5.41421 13.9722 5 13.9722H4.33333C2.07817 13.9722 0.25 12.1441 0.25 9.8889C0.25 7.8644 1.76567 6.1724 3.69762 5.858C4.28682 2.66679 7.08302 0.25 10.4444 0.25C12.947 0.25 15.1354 1.5899 16.3334 3.58865C19.2024 3.47555 21.75 5.8223 21.75 8.7778C21.75 11.4717 19.6998 13.6859 17.0741 13.9466C16.6619 13.9875 16.2946 13.6866 16.2537 13.2744C16.2127 12.8622 16.5137 12.4949 16.9259 12.4539C18.792 12.2687 20.25 10.693 20.25 8.7778C20.25 6.565 18.2032 4.80912 16.0261 5.1209C15.7057 5.1668 15.3871 5.0044 15.239 4.70953C14.3572 2.95291 12.5406 1.75 10.4444 1.75Z" fill="#94A3B8" />
@@ -223,46 +226,46 @@ function AdminAddCard() {
 											<path fillRule="evenodd" clipRule="evenodd" d="M11 16.75C11.4142 16.75 11.75 16.4142 11.75 16V10C11.75 9.5858 11.4142 9.25 11 9.25C10.5858 9.25 10.25 9.5858 10.25 10V16C10.25 16.4142 10.5858 16.75 11 16.75Z" fill="#94A3B8" />
 										</svg>
 										<span className="text-[#94A3B8] font-normal text-sm md:text-xl pl-4">
-											{formik.values.card_photo && formik.values.card_photo !== "" ?
-												formik.values.card_photo.name
+											{formik.values.frontside_card_photo && formik.values.frontside_card_photo !== "" ?
+												formik.values.frontside_card_photo.name
 												:
 												"Upload"
 											}
 										</span>
 									</label>
-									<input type="file" name="card_photo" id='card-photo' className="input_box2 placeholder:text-[#94A3B8] placeholder:text-base hidden" placeholder='upload' accept='image/*' onChange={(e) => setInputValue("card_photo", e.currentTarget.files[0])} />
-									<small className="text-red-500 text-xs">{formik.errors.card_photo}</small>
+									<input type="file" name="frontside_card_photo" id='card-photo' className="input_box2 placeholder:text-[#94A3B8] placeholder:text-base hidden" placeholder='upload' accept='image/*' onChange={(e) => setInputValue("frontside_card_photo", e.currentTarget.files[0])} />
+									<small className="text-red-500 text-xs">{formik.errors.frontside_card_photo}</small>
 								</div>
 								<div className='w-full md:w-1/2'>
-									<label htmlFor="card_photo" className="input-title2">Card back photo*</label>
-									<label className='input_box2 flex items-center border-dashed justify-start py-2' htmlFor='card-photo'>
+									<label htmlFor="backside_card_photo" className="input-title2">Card back photo*</label>
+									<label className='input_box2 flex items-center border-dashed justify-start py-2' htmlFor='card-photo-1'>
 										<svg width="22" height="17" viewBox="0 0 22 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 											<path fillRule="evenodd" clipRule="evenodd" d="M10.4444 1.75C7.65101 1.75 5.35585 3.88704 5.10594 6.6149C5.07 7.0073 4.74063 7.306 4.34837 7.3056C2.9362 7.3044 1.75 8.4797 1.75 9.8889C1.75 11.3156 2.9066 12.4722 4.33333 12.4722H5C5.41421 12.4722 5.75 12.808 5.75 13.2222C5.75 13.6364 5.41421 13.9722 5 13.9722H4.33333C2.07817 13.9722 0.25 12.1441 0.25 9.8889C0.25 7.8644 1.76567 6.1724 3.69762 5.858C4.28682 2.66679 7.08302 0.25 10.4444 0.25C12.947 0.25 15.1354 1.5899 16.3334 3.58865C19.2024 3.47555 21.75 5.8223 21.75 8.7778C21.75 11.4717 19.6998 13.6859 17.0741 13.9466C16.6619 13.9875 16.2946 13.6866 16.2537 13.2744C16.2127 12.8622 16.5137 12.4949 16.9259 12.4539C18.792 12.2687 20.25 10.693 20.25 8.7778C20.25 6.565 18.2032 4.80912 16.0261 5.1209C15.7057 5.1668 15.3871 5.0044 15.239 4.70953C14.3572 2.95291 12.5406 1.75 10.4444 1.75Z" fill="#94A3B8" />
 											<path fillRule="evenodd" clipRule="evenodd" d="M11 10.0606L12.9696 12.0302C13.2625 12.3231 13.7374 12.3231 14.0303 12.0302C14.3232 11.7373 14.3232 11.2625 14.0303 10.9696L11.8839 8.82311C11.3957 8.33501 10.6043 8.33501 10.1161 8.82311L7.96967 10.9696C7.67678 11.2625 7.67678 11.7373 7.96967 12.0302C8.26256 12.3231 8.73744 12.3231 9.0303 12.0302L11 10.0606Z" fill="#94A3B8" />
 											<path fillRule="evenodd" clipRule="evenodd" d="M11 16.75C11.4142 16.75 11.75 16.4142 11.75 16V10C11.75 9.5858 11.4142 9.25 11 9.25C10.5858 9.25 10.25 9.5858 10.25 10V16C10.25 16.4142 10.5858 16.75 11 16.75Z" fill="#94A3B8" />
 										</svg>
 										<span className="text-[#94A3B8] font-normal text-sm md:text-xl pl-4">
-											{formik.values.card_photo && formik.values.card_photo !== "" ?
-												formik.values.card_photo.name
+											{formik.values.backside_card_photo && formik.values.backside_card_photo !== "" ?
+												formik.values.backside_card_photo.name
 												:
 												"Upload"
 											}
 										</span>
 									</label>
-									<input type="file" name="card_photo" id='card-photo' className="input_box2 placeholder:text-[#94A3B8] placeholder:text-base hidden" placeholder='Card photo upload' accept='image/*' onChange={(e) => setInputValue("card_photo", e.currentTarget.files[0])} />
-									<small className="text-red-500 text-xs">{formik.errors.card_photo}</small>
+									<input type="file" name="backside_card_photo" id='card-photo-1' className="input_box2 placeholder:text-[#94A3B8] placeholder:text-base hidden" placeholder='Card photo upload' accept='image/*' onChange={(e) => setInputValue("backside_card_photo", e.currentTarget.files[0])} />
+									<small className="text-red-500 text-xs">{formik.errors.backside_card_photo}</small>
 								</div>
 							</div>
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_exp_date" className="input-title2 relative">Card Expiry Date *</label>
-								<Calendar name="due_date" className='due w-full py-[1px] overflow-hidden box-shadow' minDate={minDateValue} placeholder={new Date().toISOString().slice(0, 10)} onChange={(e) => setInputValue("due_date", moment(e.target.value).toISOString().slice(0, 10))} readOnlyInput />
-								<small className="text-red-500 text-xs">{formik.errors.due_date}</small>
+								<Calendar name="card_exp_date" className='due w-full py-[1px] overflow-hidden box-shadow' minDate={minDateValue} placeholder={new Date().toISOString().slice(0, 10)} onChange={(e) => setInputValue("card_exp_date", moment(e.target.value).toISOString().slice(0, 10))} readOnlyInput />
+								<small className="text-red-500 text-xs">{formik.errors.card_exp_date}</small>
 							</div>
 
 							{/* <div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<small className="text-red-500 text-xs">{formik.errors.card_exp_date}</small>
 								<label htmlFor="card_exp_date" className="input-title2 relative">Card Expiry Date *</label>
-								<input type="date" name="card_exp_date" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card expiry date' required onChange={(e) => setInputValue("card_exp_date", e.target.value)} />
+								<input type="date" name="card_exp_date" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter card expiry date' onChange={(e) => setInputValue("card_exp_date", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_exp_date}</small>
 								{/* <img src={CalendarIcon} alt="Calendar icon" className='absolute top-1/2 translate-y-1/2 right-10' /> 
 							</div> */}
@@ -270,7 +273,7 @@ function AdminAddCard() {
 						<div className="w-full flex flex-wrap sm:flex-nowrap sm:mb-7 sm:space-x-6">
 							<div className='w-full sm:w-1/2 mb-4 sm:mb-0'>
 								<label htmlFor="card_cvv" className="input-title2">Card CVV *</label>
-								<input maxLength={3} type="text" name="card_cvv" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter cvv' required onChange={(e) => setInputValue("card_cvv", e.target.value)} />
+								<input maxLength={3} type="text" name="card_cvv" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter cvv' onChange={(e) => setInputValue("card_cvv", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.card_cvv}</small>
 							</div>
 							{/* {formik.values.card_type === "Debit Card" ?
@@ -278,14 +281,14 @@ function AdminAddCard() {
 									{formik.values.credit_amount = ""}
 									<div className='w-full sm:w-1/2'>
 										<label htmlFor="available_balance" className="input-title2">Available balance *</label>
-										<input type="number" name="available_balance" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter available balance' required onChange={(e) => setInputValue("available_balance", e.target.value)} />
+										<input type="number" name="available_balance" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter available balance' onChange={(e) => setInputValue("available_balance", e.target.value)} />
 										<small className="text-red-500 text-xs">{formik.errors.available_balance}</small>
 									</div>
 								</>
 								: */}
 							<div className='w-full sm:w-1/2'>
 								<label htmlFor="credit_amount" className="input-title2">Credit Amount *</label>
-								<input type="number" name="credit_amount" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter credit amount' required onChange={(e) => setInputValue("credit_amount", e.target.value)} />
+								<input type="number" name="credit_amount" className="input_box2 placeholder:text-[#94A3B8] text-sm sm:placeholder:text-xl" placeholder='Enter credit amount' onChange={(e) => setInputValue("credit_amount", e.target.value)} />
 								<small className="text-red-500 text-xs">{formik.errors.credit_amount}</small>
 							</div>
 							{/* } */}
